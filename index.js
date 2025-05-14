86b5dcd16fb924abda4cb9cd9faf540e35127a06
mdalrasel <mdalrasel.dev@gmail.com>
2025-05-14 06:49:26 +0600

    second commit

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ app.get('/', (req, res) => {
     res.send('Hellooo World!')
 })
 
+const users =[
+    {id:1,name:'rakib',email:'rakib@gmail.com'},
+    {id:2,name:'shahed',email:'shahed@gmail.com'},
+    {id:3,name:'rasel',email:'rasel@gmail.com'},
+    {id:4,name:'pavel',email:'pavel@gmail.com'},
+    {id:5,name:'nesher',email:'nesher@gmail.com'},
+]
+
+app.get('/users',(req,res) =>{
+    res.send(users)
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
